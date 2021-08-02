5ea4cde0c2efdd2c822466c4d3d30fb6563802f5
Léopold Guérin <leopold.guerin@gmail.com>
2021-08-02 10:33:41 +0200

    Updates

---
+++ precache-manifest.4961898e830a34a00a452d579a56f5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e95039a2c2b1f9c030038cc7209d7a",
+    "url": "/simple-storage-react/index.html"
+  },
+  {
+    "revision": "e29859eefa544cc4b4f8",
+    "url": "/simple-storage-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "03db1bd0abd0cd90903e",
+    "url": "/simple-storage-react/static/js/2.92f64395.chunk.js"
+  },
+  {
+    "revision": "e29859eefa544cc4b4f8",
+    "url": "/simple-storage-react/static/js/main.6ff13be4.chunk.js"
+  },
+  {
+    "revision": "f7e6ef93e161d6a17497",
+    "url": "/simple-storage-react/static/js/runtime-main.44ff6f82.js"
+  }
+]);
